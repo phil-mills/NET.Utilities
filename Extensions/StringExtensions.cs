13f9eef8160480150ea373e36be0a5645837db43
Phil Mills <phil.mills@sage.com>
2023-12-04 14:38:33 +0000

    Added NullIfEmpty extension.

--- Extensions/StringExtensions.cs
+++ Extensions/StringExtensions.cs
@@ -12,6 +12,11 @@ namespace NET.Utilities.Extensions
             return s == null;
         }
 
+        public static string NullIfEmpty(this string s)
+        {
+            return string.IsNullOrEmpty(s) ? null : s;
+        }
+
         public static bool IsNullOrEmpty(this string s)
         { 
             return string.IsNullOrEmpty(s);
